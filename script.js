8b8efcb4568d38b74708c976c46ae2b4391a7219
PikriaP <102671865+PikriaP@users.noreply.github.com>
2022-11-06 02:10:52 +0400

    start

---
+++ script.js
@@ -0,0 +1,32 @@
+// N1
+let user = {
+    firstname: "giorgi",
+    lastname: "smith",
+    age: 25,
+    studentstatus: "active"
+  };
+console.log(user.studentstatus); 
+
+// N2
+
+
+
+
+
+
+// N3
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89]; 
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 5) {
+        console.log(numbers[i]);
+    }
+};
+
+// N4
+let user = {
+    name: 'giorgi',
+    age:  20,
+    studentstatus: 'active'
+}
+
